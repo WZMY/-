c39e9d3e38ff7b7b4af9ac7836585fee8bce90f0
20131105795 <a20131105795@admindeiMac-71.local>
2016-03-28 21:16:16 +0800

    绑定按钮！

---
+++ AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  简易计算器
+//
+//  Created by 20131105795 on 16/3/24.
+//  Copyright © 2016年 ROuter. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  简易计算器
+//
+//  Created by 20131105795 on 16/3/24.
+//  Copyright © 2016年 ROuter. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    @IBOutlet weak var 屏幕: NSTextField!
+    
+    @IBAction func 数字9(sender: AnyObject) {
+    }
+    
+    @IBAction func 数字8(sender: AnyObject) {
+    }
+    @IBAction func 数字7(sender: AnyObject) {
+    }
+    @IBAction func 数字6(sender: AnyObject) {
+    }
+    @IBAction func 数字5(sender: AnyObject) {
+    }
+    @IBAction func 数字4(sender: AnyObject) {
+    }
+    @IBAction func 数字3(sender: AnyObject) {
+    }
+    @IBAction func 数字2(sender: AnyObject) {
+    }
+    @IBAction func 数字1(sender: AnyObject) {
+    }
+    @IBAction func 数字0(sender: AnyObject) {
+    }
+    @IBAction func 数字点(sender: AnyObject) {
+    }
+    @IBAction func 算法加(sender: AnyObject) {
+    }
+    @IBAction func 算法减(sender: AnyObject) {
+    }
+    @IBAction func 算法乘(sender: AnyObject) {
+    }
+    @IBAction func 算法除(sender: AnyObject) {
+    }
+    @IBAction func 算法平方(sender: AnyObject) {
+    }
+    @IBAction func 算法指数幂(sender: AnyObject) {
+    }
+    @IBAction func 重置当前(sender: AnyObject) {
+    }
+    @IBAction func 重置所有(sender: AnyObject) {
+    }
+    @IBAction func 结果(sender: AnyObject) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
